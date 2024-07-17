cffdd930a637f73b4f13774fd2deb336eda0567c
elhilaamine <mohammed-amine.el-hila@polymtl.ca>
2024-07-17 10:16:01 -0400

    commit_first_files

---
+++ TP2NoteB/NoteBook/src/main/DemmareNoteBook.java
@@ -0,0 +1,11 @@
+package main;
+import javax.swing.SwingUtilities;
+
+public class DemmareNoteBook{
+	
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new EditeurDeTexte());
+    }
+	
+}
+
