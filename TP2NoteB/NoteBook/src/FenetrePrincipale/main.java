5d1d755be5c6d4e17c293ee5cf0570d993ab71e2
Sar09876 <sarafelli2006@gmail.com>
2024-07-23 14:12:46 -0400

    Create main.java

---
+++ TP2NoteB/NoteBook/src/FenetrePrincipale/main.java
@@ -0,0 +1,11 @@
+package FenetrePrincipale;
+
+import javax.swing.SwingUtilities;
+import main.EditeurDeTexte;
+
+public class main {
+        public static void main(String[] args) {
+        SwingUtilities.invokeLater(new EditeurDeTexte());
+    }
+    
+}
