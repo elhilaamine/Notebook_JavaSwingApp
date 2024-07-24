41114b1a6d426d7d0af973cf5f74205b915f727d
Sar09876 <sarafelli2006@gmail.com>
2024-07-24 14:02:37 -0400

    Update DemmareNoteBook.java

--- TP2NoteB/NoteBook/src/main/DemmareNoteBook.java
+++ TP2NoteB/NoteBook/src/main/DemmareNoteBook.java
@@ -4,6 +4,7 @@ import javax.swing.SwingUtilities;
 public class DemmareNoteBook{
 	
 	public static void main(String[] args) {
+        
         SwingUtilities.invokeLater(new EditeurDeTexte());
     }
 	
