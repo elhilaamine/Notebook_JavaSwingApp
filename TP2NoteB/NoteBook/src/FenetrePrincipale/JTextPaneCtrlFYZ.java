f124e9aa7a209fa4037e3582500ae99b6975433a
Sar09876 <sarafelli2006@gmail.com>
2024-07-24 11:32:57 -0400

    Update JTextPaneCtrlFYZ.java

--- TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
+++ TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
@@ -1,37 +1,17 @@
-package FenetrePrincipale;
-
-import java.awt.event.ActionEvent;
-import javax.swing.*;
-import javax.swing.undo.UndoManager;
-
-public class JTextPaneCtrlFYZ extends JTextPane {
-
-    private UndoManager undoManager;
+/**
+ * Classe pour JTextPane avec fonctionnalités d'annulation, de rétablissement et de recherche.
+ */
+public class JTextPaneCtrlFYZ extends JTextPaneCtrlYZ {
 
     public JTextPaneCtrlFYZ() {
-        undoManager = new UndoManager();
-        getDocument().addUndoableEditListener(undoManager);
+        super();
+        configurerRecherche();
+    }
 
-        getInputMap().put(KeyStroke.getKeyStroke("control Z"), "undo");
-        getActionMap().put("undo", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (undoManager.canUndo()) {
-                    undoManager.undo();
-                }
-            }
-        });
-
-        getInputMap().put(KeyStroke.getKeyStroke("control Y"), "redo");
-        getActionMap().put("redo", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (undoManager.canRedo()) {
-                    undoManager.redo();
-                }
-            }
-        });
-
+    /**
+     * Configure l'action de recherche (Ctrl+F).
+     */
+    private void configurerRecherche() {
         getInputMap().put(KeyStroke.getKeyStroke("control F"), "find");
         getActionMap().put("find", new AbstractAction() {
             @Override
