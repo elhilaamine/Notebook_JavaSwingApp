7ff5756fb6a772c5e0ad31b8b42a8004d0957a53
Sar09876 <sarafelli2006@gmail.com>
2024-07-24 14:02:09 -0400

    Update JTextPaneCtrlFYZ.java

--- TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
+++ TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
@@ -15,6 +15,7 @@ public class JTextPaneCtrlFYZ extends JTextPaneCtrlF {
      * </p>
      */
     public JTextPaneCtrlFYZ() {
+        
         super();
         configurerRecherche();
     }
@@ -23,15 +24,23 @@ public class JTextPaneCtrlFYZ extends JTextPaneCtrlF {
      * Configure l'action de recherche (Ctrl+F).
      */
     private void configurerRecherche() {
+
         getInputMap().put(KeyStroke.getKeyStroke("control F"), "find");
+
         getActionMap().put("find", new AbstractAction() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
+
                 String text = JOptionPane.showInputDialog(JTextPaneCtrlFYZ.this, "Entrez le texte à rechercher:");
                 if (text != null) {
+
                     int caseSensitiveOption = JOptionPane.showConfirmDialog(JTextPaneCtrlFYZ.this,
-                     "La recherche doit-elle être sensible à la casse ?", "Sensibilité à la casse", JOptionPane.YES_NO_OPTION);
+                     "La recherche doit-elle être sensible à la casse ?", 
+                     "Sensibilité à la casse", JOptionPane.YES_NO_OPTION);
+
                     boolean caseSensitive = (caseSensitiveOption == JOptionPane.YES_OPTION);
+
                     rechercher(text, caseSensitive);
                 }
             }
