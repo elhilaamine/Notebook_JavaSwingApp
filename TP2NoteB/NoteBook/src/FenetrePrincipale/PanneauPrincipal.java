34da816ba67182b513da63572657b0aaf16998de
Sar09876 <sarafelli2006@gmail.com>
2024-07-24 11:32:44 -0400

    Update PanneauPrincipal.java

--- TP2NoteB/NoteBook/src/FenetrePrincipale/PanneauPrincipal.java
+++ TP2NoteB/NoteBook/src/FenetrePrincipale/PanneauPrincipal.java
@@ -3,6 +3,9 @@ package FenetrePrincipale;
 import java.awt.*;
 import javax.swing.*;
 
+/**
+ * Panneau principal contenant la zone de texte et la barre d'état.
+ */
 public class PanneauPrincipal extends JPanel {
 
     private JTextPane zoneDeTexte;
@@ -14,12 +17,18 @@ public class PanneauPrincipal extends JPanel {
         ajouterBarreDEtat();
     }
 
+    /**
+     * Ajoute la zone de texte au panneau principal.
+     */
     private void ajouterZoneDeTexte() {
         zoneDeTexte = new JTextPane();
         JScrollPane panneauDeDefilement = new JScrollPane(zoneDeTexte);
         add(panneauDeDefilement, BorderLayout.CENTER);
     }
 
+    /**
+     * Ajoute la barre d'état au panneau principal.
+     */
     private void ajouterBarreDEtat() {
         barreDEtat = new JPanel(new BorderLayout());
         JLabel labelEtat = new JLabel("Prêt");
@@ -30,11 +39,11 @@ public class PanneauPrincipal extends JPanel {
     public JTextPane getZoneDeTexte() {
         return zoneDeTexte;
     }
-    
+
     public JPanel getBarreDEtat() {
         return barreDEtat;
     }
-    
+
     public void mettreAJourBarreDEtat(String message) {
         ((JLabel) barreDEtat.getComponent(0)).setText(message);
     }
