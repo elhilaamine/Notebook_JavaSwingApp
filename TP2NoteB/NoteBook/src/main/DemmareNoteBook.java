b199f17a87b5751b6fa7c06d0eee3ae40d60a97c
Sar09876 <sarafelli2006@gmail.com>
2024-07-31 14:58:05 -0400

    Update DemmareNoteBook.java

--- TP2NoteB/NoteBook/src/main/DemmareNoteBook.java
+++ TP2NoteB/NoteBook/src/main/DemmareNoteBook.java
@@ -1,12 +1,22 @@
 package main;
+
 import javax.swing.SwingUtilities;
 
-public class DemmareNoteBook{
-	
-	public static void main(String[] args) {
+/**
+ * La classe principale pour démarrer l'éditeur de texte.
+ * Elle utilise SwingUtilities pour garantir que l'interface utilisateur
+ * est créée et mise à jour sur le thread de l'EDT (Event Dispatch Thread).
+ */
+public class DemmareNoteBook {
+    
+    /**
+     * Méthode principale pour démarrer l'application.
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
         
+        // Utilise SwingUtilities pour lancer l'éditeur de texte sur l'EDT
         SwingUtilities.invokeLater(new EditeurDeTexte());
     }
-	
 }
-
