eb5506e35627c949c99912daf2bda445a84d9efb
Sar09876 <sarafelli2006@gmail.com>
2024-08-05 15:55:39 -0400

    Update JTextPaneCtrlFYZ.java

--- TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
+++ TP2NoteB/NoteBook/src/FenetrePrincipale/JTextPaneCtrlFYZ.java
@@ -4,19 +4,22 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 /**
- * Classe étendue pour JTextPane avec fonctionnalités d'annulation, rétablissement et recherche.
+ * Classe étendue pour JTextPane avec fonctionnalités 
+ * d'annulation, rétablissement et recherche.
  */
 public class JTextPaneCtrlFYZ extends JTextPaneCtrlF {
 
     /**
      * Constructeur par défaut de la classe JTextPaneCtrlFYZ.
-     * <p>
-     * Initialise un nouvel objet UndoManager et configure les actions d'annulation, de rétablissement et de recherche.
-     * </p>
+     * Initialise un nouvel objet UndoManager et configure 
+     * les actions d'annulation, de rétablissement et de recherche.
      */
     public JTextPaneCtrlFYZ() {
         
+        // Appelle le constructeur de la classe parent.
         super();
+        
+        // Configure l'action de recherche.
         configurerRecherche();
     }
 
@@ -25,22 +28,32 @@ public class JTextPaneCtrlFYZ extends JTextPaneCtrlF {
      */
     private void configurerRecherche() {
 
+        // Associe Ctrl+F à l'action "find".
         getInputMap().put(KeyStroke.getKeyStroke("control F"), "find");
 
+        // Définie l'action "find".
         getActionMap().put("find", new AbstractAction() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                String text = JOptionPane.showInputDialog(JTextPaneCtrlFYZ.this, "Entrez le texte à rechercher:");
+                // Affiche une boîte de dialogue pour entrer le texte à rechercher.
+                String text = JOptionPane.showInputDialog(JTextPaneCtrlFYZ.this,
+                        "Entrez le texte à rechercher:");
+                
                 if (text != null) {
 
-                    int caseSensitiveOption = JOptionPane.showConfirmDialog(JTextPaneCtrlFYZ.this,
-                     "La recherche doit-elle être sensible à la casse ?", 
-                     "Sensibilité à la casse", JOptionPane.YES_NO_OPTION);
+                    // Affiche une boîte de dialogue pour demander
+                    // la sensibilité à la casse.
+                    int caseSensitiveOption = JOptionPane.
+                    showConfirmDialog(JTextPaneCtrlFYZ.this,
+                            "La recherche doit-elle être sensible à la casse ?", 
+                            "Sensibilité à la casse", JOptionPane.YES_NO_OPTION);
 
+                    // Détermine si la recherche est sensible à la casse.
                     boolean caseSensitive = (caseSensitiveOption == JOptionPane.YES_OPTION);
 
+                    // Effectue la recherche avec ou sans sensibilité à la casse.
                     rechercher(text, caseSensitive);
                 }
             }
